b0e147a6a7c2c35a2eb250b53c53e2a5d5c0ded7
21017030 <ih021088@gmail.com>
2026-05-02 16:33:42 +0900

    Add unit selector (일/주) for duration field

--- js/main.js
+++ js/main.js
@@ -7,7 +7,13 @@ function createRow() {
     <td><input type="text" placeholder="예) A"></td>
     <td><input type="text" placeholder="예) 설계"></td>
     <td><input type="text" placeholder="예) A, B (없으면 비워두세요)"></td>
-    <td><input type="number" placeholder="예) 3" min="0"></td>
+    <td class="duration-cell">
+      <input type="number" placeholder="예) 3" min="0">
+      <select class="unit-select">
+        <option value="day">일</option>
+        <option value="week">주</option>
+      </select>
+    </td>
     <td><button class="delete-btn" title="삭제">✕</button></td>
   `;
   tr.querySelector('.delete-btn').addEventListener('click', () => tr.remove());
